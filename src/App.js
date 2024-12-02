94b4f94e6ffecd3e576eaf033762a0b45e4e8203
Naved Shaikh <shaikhnaved0001@gmail.com>
2024-12-02 21:22:21 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,49 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import './App.css';
 
+import AOS from 'aos';
+
+import Home from './home';
+import About from './about';
+import Menu from './menu';
+import Testimonial from './testimonial';
+import Events from './events';
+import Chefs from './chefs';
+import Booking from './Booking';
+import Gallery from './gallery';
+import Contact from './contact';
+import PageNotFound from './PageNotFound';
+import Layout from './layout';
+
 function App() {
+  React.useEffect(() => {
+    AOS.init();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+       <Router>
+      <Routes>
+        <Route path='/' element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path='/about' element={<About />} />
+          <Route path='/contact' element={<Contact />} />
+          <Route path='/menu' element={<Menu />} />
+          <Route path="/testimonial" element={<Testimonial />} />
+          <Route path='/events' element={<Events/>} />
+          <Route path='/chefs' element={<Chefs/>} />
+          <Route path='/Booking' element={<Booking/>} />
+
+          <Route 
+            path='/gallery' 
+            element={<Gallery/>} 
+          />
+          <Route path='*' element={<PageNotFound/>} />
+        </Route>
+      </Routes>
+    </Router>
+    </>
   );
 }
 
